7844b12b9b369424195e9170e9a3ccabb92be3e4
Janek <j.brunow@lastlevel.pl>
2023-03-08 07:42:20 +0100

    changes related with CustomAuthenticationFilter

--- src/main/java/io/getarrays/userservice/sercurity/SecurityConfig.java
+++ src/main/java/io/getarrays/userservice/sercurity/SecurityConfig.java
@@ -1,9 +1,12 @@
 package io.getarrays.userservice.sercurity;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -15,5 +18,11 @@ public class SecurityConfig
     public SecurityConfig(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userDetailsService = userDetailsService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+    protected void configure(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(null);
     }
 }
